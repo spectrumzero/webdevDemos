2e85a0aa380bf250c9b8c59c9a16f4169de5bda9
Spectrumzero <lmhspecter@gmail.com>
2024-12-18 17:30:28 +0800

    read code

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,7 @@ let questions = [
     prompt: `Inside which HTML 
                  element do we put 
                  the JavaScript?`,
-    options: [
-      "<javascript>",
-      "<js>",
-      "<script>",
-      "<scripting>",
-    ],
+    options: ["<javascript>", "<js>", "<script>", "<scripting>"],
     answer: "<script>",
   },
 
@@ -50,56 +45,33 @@ let questions = [
                  JavaScript program that
                  is used to store and 
                  retrieve data is a _____.`,
-    options: [
-      "method",
-      "assignment operator",
-      "letiable",
-      "string",
-    ],
+    options: ["method", "assignment operator", "letiable", "string"],
     answer: "letiable",
   },
 ];
 
 // Get Dom Elements
-
-let questionsEl = document.querySelector(
-  "#questions",
-);
+let questionsEl = document.querySelector("#questions");
 let timerEl = document.querySelector("#timer");
 let choicesEl = document.querySelector("#options");
-let submitBtn = document.querySelector(
-  "#submit-score",
-);
+let submitBtn = document.querySelector("#submit-score");
 let startBtn = document.querySelector("#start");
 let nameEl = document.querySelector("#name");
-let feedbackEl = document.querySelector(
-  "#feedback",
-);
-let reStartBtn = document.querySelector("#restart");
+let feedbackEl = document.querySelector("#feedback");
+// let reStartBtn = document.querySelector("#restart");
 
 // Quiz's initial state
 let currentQuestionIndex = 0;
-let time = questions.length * 15;
+let time = questions.length * 50000;
 let timerId;
 
 // Start quiz and hide frontpage
-
 function quizStart() {
-  timerId = setInterval(
-    clockTick,
-    1000,
-  );
+  timerId = setInterval(clockTick, 1000);
   timerEl.textContent = time;
-  let landingScreenEl = document.getElementById(
-    "start-screen",
-  );
-  landingScreenEl.setAttribute(
-    "class",
-    "hide",
-  );
-  questionsEl.removeAttribute(
-    "class",
-  );
+  let landingScreenEl = document.getElementById("start-screen");
+  landingScreenEl.setAttribute("class", "hide");
+  questionsEl.removeAttribute("class");
   getQuestion();
 }
 
@@ -107,38 +79,23 @@ function quizStart() {
 // Answers and create list with buttons
 function getQuestion() {
   let currentQuestion = questions[currentQuestionIndex];
-  let promptEl = document.getElementById(
-    "question-words",
-  );
+  let promptEl = document.getElementById("question-words");
   promptEl.textContent = currentQuestion.prompt;
   choicesEl.innerHTML = "";
-  currentQuestion.options.forEach(
-    function (choice, i) {
-      let choiceBtn = document.createElement(
-        "button",
-      );
-      choiceBtn.setAttribute(
-        "value",
-        choice,
-      );
-      choiceBtn.textContent = i + 1 + ". " + choice;
-      choiceBtn.onclick = questionClick;
-      choicesEl.appendChild(
-        choiceBtn,
-      );
-    },
-  );
+  currentQuestion.options.forEach(function (choice, i) {
+    let choiceBtn = document.createElement("button");
+    choiceBtn.setAttribute("value", choice);
+    choiceBtn.textContent = i + 1 + ". " + choice;
+    choiceBtn.onclick = questionClick;
+    choicesEl.appendChild(choiceBtn);
+  });
 }
 
 // Check for right answers and deduct
 // Time for wrong answer, go to next question
 
 function questionClick() {
-  if (
-    this.value !==
-      questions[currentQuestionIndex]
-        .answer
-  ) {
+  if (this.value !== questions[currentQuestionIndex].answer) {
     time -= 10;
     if (time < 0) {
       time = 0;
@@ -151,21 +108,12 @@ function questionClick() {
     feedbackEl.textContent = "Correct!";
     feedbackEl.style.color = "green";
   }
-  feedbackEl.setAttribute(
-    "class",
-    "feedback",
-  );
+  feedbackEl.setAttribute("class", "feedback");
   setTimeout(function () {
-    feedbackEl.setAttribute(
-      "class",
-      "feedback hide",
-    );
+    feedbackEl.setAttribute("class", "feedback hide");
   }, 2000);
   currentQuestionIndex++;
-  if (
-    currentQuestionIndex ===
-      questions.length
-  ) {
+  if (currentQuestionIndex === questions.length) {
     quizEnd();
   } else {
     getQuestion();
@@ -177,24 +125,14 @@ function questionClick() {
 
 function quizEnd() {
   clearInterval(timerId);
-  let endScreenEl = document.getElementById(
-    "quiz-end",
-  );
-  endScreenEl.removeAttribute(
-    "class",
-  );
-  let finalScoreEl = document.getElementById(
-    "score-final",
-  );
+  let endScreenEl = document.getElementById("quiz-end");
+  endScreenEl.removeAttribute("class");
+  let finalScoreEl = document.getElementById("score-final");
   finalScoreEl.textContent = time;
-  questionsEl.setAttribute(
-    "class",
-    "hide",
-  );
+  questionsEl.setAttribute("class", "hide");
 }
 
 // End quiz if timer reaches 0
-
 function clockTick() {
   time--;
   timerEl.textContent = time;
@@ -209,23 +147,15 @@ function clockTick() {
 function saveHighscore() {
   let name = nameEl.value.trim();
   if (name !== "") {
-    let highscores = JSON.parse(
-      window.localStorage.getItem(
-        "highscores",
-      ),
-    ) || [];
+    let highscores =
+      JSON.parse(window.localStorage.getItem("highscores")) || [];
     let newScore = {
       score: time,
       name: name,
     };
     highscores.push(newScore);
-    window.localStorage.setItem(
-      "highscores",
-      JSON.stringify(highscores),
-    );
-    alert(
-      "Your Score has been Submitted",
-    );
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
+    alert("Your Score has been Submitted");
   }
 }
 
@@ -234,9 +164,7 @@ function saveHighscore() {
 function checkForEnter(event) {
   if (event.key === "Enter") {
     saveHighscore();
-    alert(
-      "Your Score has been Submitted",
-    );
+    alert("Your Score has been Submitted");
   }
 }
 nameEl.onkeyup = checkForEnter;
